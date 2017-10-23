8f91c207ca56bf1ea6b1c79887c386f904699b1e
Grace BK <iris.b1991@gmail.com>
2017-10-23 11:51:02 +0200

    Liste monomorphe statique/dynamique
    Dans cette deuxième version (MonoMultiList), la classe est générique au sens de Java 5, et elle hérite de ArrayList<T>. Les choses sont un peu plus compliquées car on veut pouvoir faire fonctionner la classe en mode monomorphe (booléen isMono à vrai) ou en mode polymorphe classique.
    De la même manière que précédemment, il vous faut :
    compléter la classe,
    réfléchir à comment la tester,
    vérifier votre intuition, compléter la classe de test et passer les tests.
    Observez bien les commentaires d'explication déjà placés dans les classes et observez aussi quand sont effectuées les vérifications (à la compilation ou à l'exécution).

--- src/grace/bk/util/MonoMultiList.java
+++ src/grace/bk/util/MonoMultiList.java
@@ -7,12 +7,15 @@ public class MonoMultiList<T> extends ArrayList<T> {
 	protected Class<?> protoClass = null; // Classe du prototype
 	protected boolean isMono; // isMono est vrai si la liste est monomorphe
 
-	public MonoMultiList(T proto) { // Le prototype est fix� � la cr�ation, mais uniquement
-		// par une instance, car sinon, on ne pourrait pas v�rifier que le type pass� en 
-		// param�tre est un sous-type de T (impossibilit� de Java 5 car T disparait � la
-		// compilation par n�cessit� de compatibilit�)
+	public MonoMultiList(T proto) { // Le prototype est fixe e la creation, mais uniquement
+		// par une instance, car sinon, on ne pourrait pas verifier que le type passe en 
+		// parametre est un sous-type de T (impossibilite de Java 5 car T disparait e la
+		// compilation par necessite de compatibilite)
 		super();
-        // TODO placer l'objet dans la liste et s'en servir comme prototype		
+        // TODO placer l'objet dans la liste et s'en servir comme prototype
+        // DONE
+        super.add(proto);
+        protoClass = proto.getClass();
 		isMono = true;  // liste monomorphe stricte
 	}
 	
@@ -24,11 +27,29 @@ public class MonoMultiList<T> extends ArrayList<T> {
 	public Class<?> getProtoClass() {
 		return protoClass;
 	}
-	
+
 	public boolean add(T t) {
-		// TODO si on est mode Monomorphe, v�rifier la coh�rence avec la classe prototype
-		// TODO ne pas oublier de g�rer le bool�en (vrai si l'ajout s'est bien fait)
-		return true;
-	}
+        // TODO si on est mode Monomorphe, verifier la coherence avec la classe prototype
+        // TODO ne pas oublier de gerer le booleen (vrai si l'ajout s'est bien fait)
+
+        /*
+        dans multi on ajoute dans la liste si c'est mono et si la coherence est verifie
+        entre la l'Objet (T) et la classe prototype
+        Si c'est pas mono en
+         */
+
+        //return isMono && (getProtoClass() == t.getClass()) && super.add(t);
+
+        //return !isMono || getProtoClass() == t.getClass() && super.add(t);
+
+        if (isMono) { // Cas mono
+            if ((getProtoClass() == t.getClass())) {
+                return super.add(t);
+            }
+        } else { // Cas multi
+            return super.add(t);
+        }
+        return false;
+    }
 	
 }
--- src/grace/bk/util/test/MonoMultiListTest.java
+++ src/grace/bk/util/test/MonoMultiListTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import grace.bk.util.MonoMultiList;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class MonoMultiListTest {
 
 	private MonoMultiList<A> monol;
@@ -20,36 +23,45 @@ public class MonoMultiListTest {
 	public void testAddMono_OK() {
 		// TODO 
 		// assertTrue("ajout d'un B dans une liste<A> monomorphe<B>", ??? );
+        assertTrue(monol.add(new B()));
 	}
 	
 	@Test
 	public void testAddMono_NOK_up() {
 //		 TODO 
 		// assertFalse("ajout d'un A dans une liste<A> monomorphe<B>", ??? );
+        assertFalse(monol.add(new A()));
 	}
 	
 	@Test
 	public void testAddMono_NOK_down() {
 //		 TODO 
 		// assertFalse("ajout d'un C dans une liste<A> monomorphe<B>", ??? );
+        assertFalse(monol.add(new C()));
 	}
 	
 	@Test
 	public void testAddMulti_OK_eq() {
 //		 TODO 
 		// assertTrue("ajout d'un B dans une liste<B> polymorphe", ??? );
+        assertTrue(multil.add(new B()));
 	}
-	
+
 	@Test
 	public void testAddMulti_NOK_up() {
 //		 TODO 
 		// assertFalse("ajout d'un A dans une liste<B> polymorphe", ??? );
+        /*
+        Le test ne passe pas car A est la classe mere
+         */
+        //assertFalse(multil.add(new A()));
 	}
 	
 	@Test
 	public void testAddMulti_OK_down() {
 //		 TODO 
 		//assertTrue("ajout d'un C dans une liste<B> polymorphe", ??? );
+        assertTrue(multil.add(new C()));
 	}
 
 	class A {

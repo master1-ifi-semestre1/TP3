ec255f51c283bf7a9c7269b3e163db0e74ba413e
Grace BK <iris.b1991@gmail.com>
2017-10-23 10:53:50 +0200

    Liste monomorphe dynamique

--- src/grace/bk/util/MonoDynList.java
+++ src/grace/bk/util/MonoDynList.java
@@ -9,12 +9,17 @@ public class MonoDynList extends ArrayList<Object> {
 	public MonoDynList(Class protoClass) {
 		super();
 		// TODO conserver le prototype
+        // DONE
+		this.protoClass = protoClass;
 		
 	}
 	
 	public MonoDynList(Object proto) {
 		super();
 		// TODO placer l'objet dans la liste et s'en servir comme prototype
+        // DONE
+		super.add(proto);
+		protoClass = proto.getClass();
 		
 	}
 	
@@ -25,7 +30,12 @@ public class MonoDynList extends ArrayList<Object> {
 	public boolean add(Object t) {
 		// TODO v�rifier la coh�rence avec la classe prototype
 		// TODO ne pas oublier de g�rer le bool�en (vrai si l'ajout s'est bien fait)
-		return true;
+        if (getProtoClass() == t.getClass()) {
+            return super.add(t);
+        } else {
+            return false;
+        }
+		// return true;
 	}
 
 }
--- src/grace/bk/util/test/MonoDynListTest.java
+++ src/grace/bk/util/test/MonoDynListTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import grace.bk.util.MonoDynList;
 
+import static org.junit.Assert.assertTrue;
+
 public class MonoDynListTest {
 
 	private MonoDynList mdl;
@@ -12,24 +14,29 @@ public class MonoDynListTest {
 	@Before
 	public void setUp() throws Exception {
 		// TODO cr�er une liste de B
+		// DONE
+		mdl = new MonoDynList(new B());
 	}
 
 	@Test
 	public void testAdd_OK() {
 		// TODO 
 		// assertTrue("ajout d'un B dans une liste monomorphe de B", ??? );
+		assertTrue(mdl.add(new B()));
 	}
 	
 	@Test
 	public void testAdd_NOK_up() {
 		// TODO
 		// assertFalse("ajout d'un A dans une liste monomorphe de B", ??? );
+        assertTrue(mdl.add(new A()));
 	}
 
 	@Test
 	public void testAdd_NOK_down() {
 		// TODO
 		// assertFalse("ajout d'un C dans une liste monomorphe de B", ??? );
+        assertTrue(mdl.add(new C()));
 	}
 
 	class A {
